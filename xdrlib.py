6d5d2ca448d11f914f0b2b62a6b3f5cd534857b7
David Arnold <d@0x1.org>
2024-04-06 00:14:51 +1100

    Cleanup: docstrings, f-strings, comments, etc.

--- xdrlib.py
+++ xdrlib.py
@@ -4,18 +4,21 @@ See: RFC 1014
 
 """
 
-import struct
 from io import BytesIO
 from functools import wraps
-import warnings
+import struct
+import typing
+import warning
 
 # Package version
 __version__ = "4.0.2"
 
 
+# Control * exports.
 __all__ = ["Error", "Packer", "Unpacker", "ConversionError"]
 
-# exceptions
+
+# Exceptions
 class Error(Exception):
     """Exception class for this module. Use:
 
@@ -35,7 +38,8 @@ class Error(Exception):
 
 
 class ConversionError(Error):
-    pass
+    """Error during conversion to or from XDR format."""
+
 
 def raise_conversion_error(function):
     """ Wrap any raised struct.errors in a ConversionError. """
@@ -53,29 +57,47 @@ class Packer:
     """Pack various data representations into a buffer."""
 
     def __init__(self):
+        """Create an XDR "packer" that can encode Python data into XDR format."""
         self.reset()
 
     def reset(self):
+        """Reset the internal buffer for packed data."""
         self.__buf = BytesIO()
 
-    def get_buffer(self):
+    def get_buffer(self) -> bytes:
+        """Return the packed data."""
         return self.__buf.getvalue()
-    # backwards compatibility
-    get_buf = get_buffer
+
+    def get_buf(self) -> bytes:
+        """(Deprecated) Return the packed data.
+
+        This function is deprecated.  Use get_buffer() instead."""
+
+        warnings.warn("xdrlib.Packet.get_buf() is deprecated. Use get_buffer() instead",
+                      DeprecationWarning)
+
+        return self.get_buffer()
 
     @raise_conversion_error
-    def pack_uint(self, x):
+    def pack_uint(self, x: int):
+        """Pack a 32-bit unsigned integer value."""
         self.__buf.write(struct.pack('>L', x))
 
     @raise_conversion_error
-    def pack_int(self, x):
+    def pack_int(self, x: int):
+        """Pack a 32-bit signed integer value."""
         self.__buf.write(struct.pack('>l', x))
 
-    pack_enum = pack_int
+    def pack_enum(self, x: int):
+        """Pack an enumeration value."""
+        self.pack_int(x)
 
-    def pack_bool(self, x):
-        if x: self.__buf.write(b'\0\0\0\1')
-        else: self.__buf.write(b'\0\0\0\0')
+    def pack_bool(self, x: typing.Any):
+        """Pack a boolean value."""
+        if x:
+            self.__buf.write(b'\0\0\0\1')
+        else:
+            self.__buf.write(b'\0\0\0\0')
 
     @raise_conversion_error
     def pack_uhyper(self, x: int):
@@ -88,92 +110,195 @@ class Packer:
         self.__buf.write(struct.pack(">q", x))
 
     @raise_conversion_error
-    def pack_float(self, x):
+    def pack_float(self, x: float):
+        """Pack a 32-bit (single-precision) floating point value."""
         self.__buf.write(struct.pack('>f', x))
 
     @raise_conversion_error
-    def pack_double(self, x):
+    def pack_double(self, x: float):
+        """Pack a 64-bit (double-precision) floating point value."""
         self.__buf.write(struct.pack('>d', x))
 
-    def pack_fstring(self, n, s):
-        if n < 0:
-            raise ValueError('fstring size must be nonnegative')
-        data = s[:n]
-        n = ((n+3)//4)*4
-        data = data + (n - len(data)) * b'\0'
+    def pack_fstring(self, length: int, data: bytes):
+        """Pack a fixed-size string value.
+
+        In XDR, the "string" type has no real distinction from
+        the opaque type.  Both are a sequence of bytes.  When
+        writing a Python string, it should be encoded first,
+        typically as UTF-8.
+
+        packer.pack_fstring(7, str_value.encode("UTF-8"))
+
+        A fixed string value has a fixed length, pre-defined and
+        known to both sender and receiver.  The length is not sent
+        over the wire, just the data bytes.
+
+        :param length: size of the input, in bytes
+        :param data: bytes value, at least "length" bytes long"""
+
+        return self.pack_fopaque(length, data)
+
+    def pack_fopaque(self, length: int, data: bytes):
+        """Pack a fixed-size byte buffer value.
+
+        A fixed opaque value has a fixed length, pre-defined and
+        known to both sender and receiver.  The length is not sent
+        over the wire, just the data bytes.
+
+        :param length: size of the input, in bytes
+        :param data: bytes value"""
+
+        if length < 0:
+            raise ValueError(f"data size {length} must not "
+                             "be negative")
+        elif len(data) < length:
+            raise ValueError(f"data size {len(data)} less than "
+                             f"specified size {length}")
+
+        data = data[:length]
+        length = ((length + 3) // 4) * 4
+        data = data + (length - len(data)) * b'\0'
         self.__buf.write(data)
 
-    pack_fopaque = pack_fstring
+    def pack_opaque(self, data: bytes):
+        """Pack an opaque value.
 
-    def pack_string(self, s):
-        n = len(s)
-        self.pack_uint(n)
-        self.pack_fstring(n, s)
+        An XDR opaque value is a sequence of uninterpreted bytes.
 
-    pack_opaque = pack_string
-    pack_bytes = pack_string
+        :param data: bytes value to pack."""
+        length = len(data)
+        self.pack_uint(length)
+        self.pack_fstring(length, data)
 
-    def pack_list(self, list, pack_item):
-        for item in list:
+    def pack_string(self, data: bytes):
+        """Pack a string value.
+
+        In XDR, the "string" type has no real distinction from
+        the opaque type.  Both are a sequence of bytes.  When
+        writing a Python string, it should be encoded first,
+        typically as UTF-8.
+
+        packer.pack_string(str_value.encode("UTF-8"))
+
+        An XDR variable-length string value is encoded on the
+        wire as a 32-bit integer length prior to the contents
+        of the string itself.
+
+        :param data: bytes value to pack"""
+        self.pack_opaque(data)
+
+    def pack_bytes(self, data: bytes):
+        """Pack a Python bytes value.
+
+        Bytes values are packed as an XDR opaque.
+
+        :param data: bytes value to pack."""
+        self.pack_opaque(data)
+
+
+    def pack_list(self,
+                  seq: typing.Sequence,
+                  pack_item: typing.Callable):
+        """Pack a list of items.
+
+        :param seq: List of items to be packed
+        :param pack_item: Function to use to pack items"""
+        for item in seq:
             self.pack_uint(1)
             pack_item(item)
         self.pack_uint(0)
 
-    def pack_farray(self, n, list, pack_item):
-        if len(list) != n:
+    def pack_farray(self, length: int,
+                    seq: typing.Sequence,
+                    pack_item: typing.Callable):
+        """Pack a fixed-size array of items.
+
+        :param length: Integer number of items
+        :param seq: List of items to be packed
+        :param pack_item: Function to use to pack items"""
+        if len(seq) != length:
             raise ValueError('wrong array size')
-        for item in list:
+        for item in seq:
             pack_item(item)
 
-    def pack_array(self, list, pack_item):
-        n = len(list)
-        self.pack_uint(n)
-        self.pack_farray(n, list, pack_item)
+    def pack_array(self,
+                   seq: typing.Sequence,
+                   pack_item: typing.Callable):
+        """Pack a variable-length array of items.
 
+        :param seq: List of items to be packed
+        :param pack_item: Function to use to pack items"""
+        length = len(seq)
+        self.pack_uint(length)
+        self.pack_farray(length, seq, pack_item)
 
 
 class Unpacker:
     """Unpacks various data representations from the given buffer."""
 
-    def __init__(self, data):
-        self.reset(data)
-
-    def reset(self, data):
+    def __init__(self, data: bytes):
+        """Create an XDR decoder for the supplied buffer."""
         self.__buf = data
         self.__pos = 0
 
-    def get_position(self):
+    def reset(self, data):
+        """Reset the XDR decoder.
+
+        :param data: Bytes buffer to be decoded"""
+        self.__buf = data
+        self.__pos = 0
+
+    def get_position(self) -> int:
+        """Return the current offset within the decoding buffer."""
         return self.__pos
 
-    def set_position(self, position):
+    def set_position(self, position: int):
+        """Set the position within the decoding buffer.
+
+        :param position: Integer offset of next position to be decoded from the buffer."""
         self.__pos = position
 
-    def get_buffer(self):
+    def get_buffer(self) -> bytes:
+        """Return the complete internal buffer to be decoded."""
         return self.__buf
 
     def done(self):
+        """Assert that the decoding is complete."""
         if self.__pos < len(self.__buf):
             raise Error('unextracted data remains')
 
-    def unpack_uint(self):
+    def unpack_uint(self) -> int:
+        """Decode a 32-bit unsigned integer value at the current position.
+
+        Advances the internal position by 4 octets."""
         i = self.__pos
-        self.__pos = j = i+4
+        self.__pos = j = i + 4
         data = self.__buf[i:j]
         if len(data) < 4:
             raise EOFError
         return struct.unpack('>L', data)[0]
 
-    def unpack_int(self):
+    def unpack_int(self) -> int:
+        """Decode a 32-bit signed integer value at the current position.
+
+        Advances the internal position by 4 octets."""
         i = self.__pos
-        self.__pos = j = i+4
+        self.__pos = j = i + 4
         data = self.__buf[i:j]
         if len(data) < 4:
             raise EOFError
         return struct.unpack('>l', data)[0]
 
-    unpack_enum = unpack_int
+    def unpack_enum(self) -> int:
+        """Decode an enumeration value.
 
-    def unpack_bool(self):
+        Advances the internal position by 4 octets."""
+        return self.unpack_int()
+
+    def unpack_bool(self) -> bool:
+        """Decode a boolean value at the current position.
+
+        Advances the internal position by 4 octets."""
         return bool(self.unpack_int())
 
     def unpack_uhyper(self) -> int:
@@ -198,8 +323,10 @@ class Unpacker:
             raise EOFError
         return struct.unpack('>q', data)[0]
 
+    def unpack_float(self) -> float:
+        """Decode a 32-bit (single precision) floating point value at the current position.
 
-    def unpack_float(self):
+        Advances the internal position by 4 octets."""
         i = self.__pos
         self.__pos = j = i+4
         data = self.__buf[i:j]
@@ -207,7 +334,10 @@ class Unpacker:
             raise EOFError
         return struct.unpack('>f', data)[0]
 
-    def unpack_double(self):
+    def unpack_double(self) -> float:
+        """Decode a 364-bit (double precision) floating point value at the current position.
+
+        Advances the internal position by 4 octets."""
         i = self.__pos
         self.__pos = j = i+8
         data = self.__buf[i:j]
@@ -215,7 +345,8 @@ class Unpacker:
             raise EOFError
         return struct.unpack('>d', data)[0]
 
-    def unpack_fstring(self, n):
+    def unpack_fstring(self, n: int):
+        """Decode a fixed-length string at the current position."""
         if n < 0:
             raise ValueError('fstring size must be nonnegative')
         i = self.__pos
@@ -228,29 +359,41 @@ class Unpacker:
     unpack_fopaque = unpack_fstring
 
     def unpack_string(self):
+        """Decode a variable-length string from the current position."""
         n = self.unpack_uint()
         return self.unpack_fstring(n)
 
     unpack_opaque = unpack_string
     unpack_bytes = unpack_string
 
-    def unpack_list(self, unpack_item):
-        list = []
+    def unpack_list(self, unpack_item: typing.Callable) -> typing.Sequence:
+        """Decode a variable-length list.
+
+        :param unpack_item: Function to decode items"""
+        seq = []
         while 1:
             x = self.unpack_uint()
-            if x == 0: break
+            if x == 0:
+                break
             if x != 1:
-                raise ConversionError('0 or 1 expected, got %r' % (x,))
+                raise ConversionError(f"0 or 1 expected, got {x}")
             item = unpack_item()
-            list.append(item)
-        return list
+            seq.append(item)
+        return seq
 
-    def unpack_farray(self, n, unpack_item):
-        list = []
+    def unpack_farray(self, n: int, unpack_item: typing.Callable) -> typing.Sequence:
+        """Decode fixed-length array.
+
+        :param n: Integer number of elements
+        :param unpack_item: Function to decode items"""
+        seq = []
         for i in range(n):
-            list.append(unpack_item())
-        return list
+            seq.append(unpack_item())
+        return seq
 
-    def unpack_array(self, unpack_item):
+    def unpack_array(self, unpack_item: typing.Callable) -> typing.Sequence:
+        """Decode array of items.
+
+        :param unpack_item: Function to unpack array elements"""
         n = self.unpack_uint()
         return self.unpack_farray(n, unpack_item)
